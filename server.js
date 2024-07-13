780725fbcbb639fddf62c8c9cbfcf718bfc79d26
meet2901 <138597918+meet2901@users.noreply.github.com>
2024-07-13 18:34:13 +0530

    nodeJS  pamplate

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const db = require('./db');
+const app = express();
+require('dotenv').config();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+const PORT=process.env.PORT||3500
+// const PORT=3500;
+app.get('/',(req,res)=>{
+    res.send('welcome to nayana ben hotels ')
+})
+
+const personRouter = require('./routers/personRouter');
+
+app.use('/person', personRouter);
+const menuRouter = require('./routers/menuRouter');
+
+app.use('/menu', menuRouter);
+
+app.listen(PORT, () => {
+    console.log(`server is listing on port 3500`);
+});
+//
